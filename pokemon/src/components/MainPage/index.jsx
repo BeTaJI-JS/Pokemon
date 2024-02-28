00fbb953d66a8c5716e458ded575e351992bd524
BeTaJI <lilysh994@mail.ru>
2024-02-28 14:58:41 +0300

    прототип виртуализации, делал по одному из роликов ютуба, скролл зацикливается при том что я делаю очистку состояния,не совсем понятно что происходит

--- pokemon/src/components/MainPage/index.jsx
+++ pokemon/src/components/MainPage/index.jsx
@@ -1,18 +1,101 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { useGetItemsQuery } from "../../store/api";
 import { Link, useNavigate } from "react-router-dom";
-import logo from '../../assets/pokedex_logo.png'
+import logo from "../../assets/pokedex_logo.png";
 
+const itemHeight = 40;
+const containerHeight = 1000;
+const overscan  = 3;
 
 export const MainPage = () => {
-  const [page, setPage] = useState(1); // Состояние для отслеживания номера страницы
+  // const [page, setPage] = useState(1); // Состояние для отслеживания номера страницы
   const [allPokemons, setAllPokemons] = useState([]); // любоптынй факт, если делать через useMemo  - не отрисовывается
+  const [scrollTop, setScrollTop] = useState(0);
+  const [isScrolling, setIsScrolling] = useState(false);
+
+
+  const scrollElementRef = useRef(null);
+
+  useLayoutEffect(() => {
+    const scrollElement = scrollElementRef.current;
+    if (!scrollElement) {
+      return;
+    }
+    const handleScroll = () => {
+      const scrollTop = scrollElement.scrollTop;
+      setScrollTop(scrollTop);
+    };
+    handleScroll();
+
+    scrollElement.addEventListener("scroll", handleScroll);
+
+    return () => scrollElement.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
+   useEffect(() => {
+     const scrollElement = scrollElementRef.current;
+     if (!scrollElement) {
+       return;
+     }
+
+     let timeoutId = null
+     const handleScroll = () => {
+      setIsScrolling(true);
+      if(typeof timeoutId === 'number'){
+        clearTimeout(timeoutId)
+      }
+
+      timeoutId = setTimeout(()=> {
+      setIsScrolling(false);
+      },100)
+     };
+
+
+     scrollElement.addEventListener("scroll", handleScroll);
+
+     return () => {
+       if (typeof timeoutId === "number") {
+         clearTimeout(timeoutId);
+       }
+      scrollElement.removeEventListener("scroll", handleScroll)}
+   }, []);
+
+   console.log('isScrolling--->',isScrolling);
+  // const [startIndex, endIndex] = useMemo(() => {
+  const virtualItems = useMemo(() => {
+    const rangeStart = scrollTop;
+    const rangeEnd = scrollTop + containerHeight;
+
+    let startIndex = Math.floor(rangeStart / itemHeight);
+    let endIndex = Math.floor(rangeEnd / itemHeight);
+
+    startIndex = Math.max(0, startIndex - overscan);
+    endIndex = Math.min(allPokemons.length - 1, endIndex + overscan);
+
+    const virtualItems = []
+    for(let i = startIndex; i < endIndex; i ++){
+virtualItems.push({
+  index: i,
+  offsetTop: i * itemHeight,
+})
+    }
+
+    return virtualItems;
+  }, [scrollTop, allPokemons.length]);
 
   const navigate = useNavigate();
-  const { data = [], isFetching } = useGetItemsQuery({});// приходит только имя и ссылка
+  const { data = [], isFetching } = useGetItemsQuery({}); // приходит только имя и ссылка
 
   const pokemons = useMemo(() => data?.results, [data]);
-console.log("pokemons", pokemons);
+  console.log("pokemons", pokemons);
   const loadPokemons = useCallback(async () => {
     for (var i = 0; i < pokemons?.length; i++) {
       await fetch(pokemons[i].url)
@@ -39,28 +122,28 @@ console.log("pokemons", pokemons);
     }
   }, [loadPokemons, data]);
 
-  useEffect(() => {
-    //e.target.documentElement.scrollHeight – высота всего скролла;
-    // e.target.documentElement.scrollTop – сколько мы уже прокрутили от верхней части;
-    // window.innerHeight – высота видимой части страницы.
+  // useEffect(() => {
+  //   //e.target.documentElement.scrollHeight – высота всего скролла;
+  //   // e.target.documentElement.scrollTop – сколько мы уже прокрутили от верхней части;
+  //   // window.innerHeight – высота видимой части страницы.
 
-    const handleScroll = () => {
-      // console.log(" window.innerHeight===>", window.innerHeight);
-      if (
-        window.innerHeight + window.scrollY ===
-          document.documentElement.offsetHeight &&
-        !isFetching
-      ) {
-        console.log("page", page);
-        setPage((prevPage) => prevPage + 1); // Увеличиваем номер страницы при достижении конца страницы
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
+  //   const handleScroll = () => {
+  //     // console.log(" window.innerHeight===>", window.innerHeight);
+  //     if (
+  //       window.innerHeight + window.scrollY ===
+  //         document.documentElement.offsetHeight &&
+  //       !isFetching
+  //     ) {
+  //       console.log("page", page);
+  //       setPage((prevPage) => prevPage + 1); // Увеличиваем номер страницы при достижении конца страницы
+  //     }
+  //   };
+  //   window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [isFetching, page]);
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, [isFetching, page]);
 
   console.log("allPokemons-->", allPokemons);
 
@@ -71,6 +154,8 @@ console.log("pokemons", pokemons);
     [navigate]
   );
 
+  // const itemsToRender = allPokemons.slice(startIndex, endIndex +1)
+// console.log("itemsToRender_______>", itemsToRender);
   return (
     <>
       <div>
@@ -79,11 +164,21 @@ console.log("pokemons", pokemons);
         </Link>
       </div>
       <h1>ПОКЕМОНЫ ЕПТА</h1>
-      <div>
-        {allPokemons?.map((pokemon) => (
+      <div
+        ref={scrollElementRef}
+        style={{
+          height: containerHeight,
+          overflow: "auto",
+          border: "3px solid red",
+          position:'relative',
+        }}
+      >
+        {virtualItems?.map((virtualItem) => {
+          const pokemon = allPokemons[virtualItem.index]
+          return(
           <div
             key={pokemon.id}
-            style={{ color: "red" }}
+            style={{ color: "red", top:0 , transform:`translateY(${pokemon.offsetTop})px` }}
             onClick={() => {
               clickHandler(pokemon.id);
             }}
@@ -91,7 +186,7 @@ console.log("pokemons", pokemons);
             <div>{pokemon.name}</div>
             <img src={pokemon.image} alt={`${pokemon.name}`} />
           </div>
-        ))}
+        )})}
         {isFetching && <div>Loading...</div>}
       </div>
     </>
