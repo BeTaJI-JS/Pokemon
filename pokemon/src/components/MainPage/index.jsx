1b216101679450ba4f7b41f34c99be3ab173836e
BeTaJI <lilysh994@mail.ru>
2024-02-29 15:24:57 +0300

    Сделал рабочим скролл с запросом данных. пришлось отказаться от логики виртуализации т к она не подходила теперь надо придумать че делать с виртуализацией

--- pokemon/src/components/MainPage/index.jsx
+++ pokemon/src/components/MainPage/index.jsx
@@ -2,7 +2,7 @@ import {
   useCallback,
   useEffect,
   useLayoutEffect,
-  useMemo,
+  // useMemo,
   useRef,
   useState,
 } from "react";
@@ -11,26 +11,37 @@ import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/pokedex_logo.png";
 
 const itemHeight = 40;
-const containerHeight = 400;
-const overscan = 3;
+const containerHeight = 700;
+// const overscan = 3;
+
+const threshold = 700
 
 export const MainPage = () => {
   // const [page, setPage] = useState(1); // Состояние для отслеживания номера страницы
-  // const [allPokemons, setAllPokemons] = useState([]); // любоптынй факт, если делать через useMemo  - не отрисовывается
+  // const [fullData, setfullData] = useState([]); // любоптынй факт, если делать через useMemo  - не отрисовывается
   const [scrollTop, setScrollTop] = useState(0);
-  const [isScrolling, setIsScrolling] = useState(false);
+  const [, setIsScrolling] = useState(false);
+  const [fullData, setFullData] = useState([]);
 
-  const [limit] = useState(100)
+  console.log("fullData--->>>", fullData);
+
+  const [limit] = useState(20);
   const [offset, setOffset] = useState(0);
-  console.log("offset--->>>>", offset);
-console.log("scrollTop-->", scrollTop);
+  //   console.log("offset--->>>>", offset);
+  // console.log("scrollTop-->", scrollTop);
   const scrollElementRef = useRef(null);
 
-    const navigate = useNavigate();
-    const { data = [], isFetching } = useGetItemsQuery({limit , offset}); // приходит только имя и ссылка
+  const navigate = useNavigate();
+  const { data = null, isFetching } = useGetItemsQuery({ limit, offset }); // приходит только имя и ссылка
 
-    const allPokemons = useMemo(() => data?.results, [data]);
-    // const allPokemons = pokemons;
+  // const fullData = useMemo(() => data?.results, [data]);
+  // const fullData = pokemons;
+
+  useEffect(() => {
+    if (data?.results?.length > 0) {
+      setFullData((prev) => [...prev, ...data.results]);
+    }
+  }, [data?.results]);
 
   useLayoutEffect(() => {
     const scrollElement = scrollElementRef.current;
@@ -59,12 +70,12 @@ console.log("scrollTop-->", scrollTop);
     }
 
     const handleScroll = () => {
-      console.log("scrollElement.clientHeight==>>>", scrollElement.clientHeight);
-      console.log("scrollElement.scrollTop==>>>", scrollElement.scrollTop);
-      console.log(
-        "!!!!===>>>scrollElement.scrollHeight-->>>",
-        scrollElement.scrollHeight
-      );
+      // console.log("scrollElement.clientHeight==>>>", scrollElement.clientHeight);
+      // console.log("scrollElement.scrollTop==>>>", scrollElement.scrollTop);
+      // console.log(
+      //   "!!!!===>>>scrollElement.scrollHeight-->>>",
+      //   scrollElement.scrollHeight
+      // );
       setIsScrolling(true);
 
       clearTimeout(scrollTimeout);
@@ -73,13 +84,13 @@ console.log("scrollTop-->", scrollTop);
 
         if (
           scrollElement.clientHeight + scrollElement.scrollTop >=
-          scrollElement.clientHeight
+          scrollElement.scrollHeight - threshold
         ) {
-          alert("asasf");
+          // alert("Скрол ниже рамки клиента - добавляю offset в запрос");
           // Достигли конца списка, увеличиваем offset
           setOffset((prevOffset) => prevOffset + limit);
         }
-      }, 150); // таймаут, чтобы определить остановку скролла
+      }, 500); // таймаут, чтобы определить остановку скролла
     };
 
     let scrollTimeout;
@@ -89,6 +100,7 @@ console.log("scrollTop-->", scrollTop);
     return () => {
       clearTimeout(scrollTimeout);
       scrollElement.removeEventListener("scroll", handleScroll);
+      // setFullData([])
     };
   }, [limit, offset]);
 
@@ -117,27 +129,28 @@ console.log("scrollTop-->", scrollTop);
   //   };
   // }, []);
 
+  //TODO virtualItems - не работает корректно НО при скроллинге дает новые данные по запросу корреткно
   // const [startIndex, endIndex] = useMemo(() => {
-  const virtualItems = useMemo(() => {
-    const rangeStart = scrollTop;
-    const rangeEnd = scrollTop + containerHeight;
+  // const virtualItems = useMemo(() => {
+  //   const rangeStart = scrollTop;
+  //   const rangeEnd = scrollTop + containerHeight;
 
-    let startIndex = Math.floor(rangeStart / itemHeight);
-    let endIndex = Math.floor(rangeEnd / itemHeight);
+  //   let startIndex = Math.floor(rangeStart / itemHeight);
+  //   let endIndex = Math.floor(rangeEnd / itemHeight);
 
-    startIndex = Math.max(0, startIndex - overscan);
-    endIndex = Math.min(allPokemons?.length - 1, endIndex + overscan);
+  //   startIndex = Math.max(0, startIndex - overscan);
+  //   endIndex = Math.min(fullData?.length - 1, endIndex + overscan);
 
-    const virtualItems = [];
-    for (let i = startIndex; i < endIndex; i++) {
-      virtualItems.push({
-        index: i,
-        offsetTop: i * itemHeight,
-      });
-    }
+  //   const virtualItems = [];
+  //   for (let i = startIndex; i < endIndex; i++) {
+  //     virtualItems.push({
+  //       index: i,
+  //       offsetTop: i * itemHeight,
+  //     });
+  //   }
 
-    return virtualItems;
-  }, [scrollTop, allPokemons?.length]);
+  //   return virtualItems;
+  // }, [scrollTop, fullData?.length]);
 
   const clickHandler = useCallback(
     (id) => {
@@ -146,9 +159,9 @@ console.log("scrollTop-->", scrollTop);
     [navigate]
   );
 
-  // const itemsToRender = allPokemons.slice(startIndex, endIndex +1)
+  // const itemsToRender = fullData.slice(startIndex, endIndex +1)
   // console.log("itemsToRender_______>", itemsToRender);
-  const totalListHeight = allPokemons?.length > 0 && itemHeight * allPokemons?.length;
+  const totalListHeight = fullData?.length > 0 && itemHeight * fullData?.length;
 
   console.log("totalListHeight--->>>", totalListHeight);
   return (
@@ -163,18 +176,22 @@ console.log("scrollTop-->", scrollTop);
         ref={scrollElementRef}
         style={{
           height: containerHeight,
-          overflow: "auto",
+          overflowY: "scroll",
           border: "3px solid red",
           position: "relative",
         }}
       >
-          <div style={{height: totalListHeight}}>
-        { virtualItems?.map((virtualItem) => {
-          const pokemon = allPokemons[virtualItem.index];
-          return !isScrolling ?
-              <div
+        {/* <div> */}
+        {fullData?.map((pokemon) => {
+          // const pokemon = fullData[virtualItem.index];
+          return (
+            <div
               key={pokemon.name}
               style={{
+                height: itemHeight,
+                boxSizing: "border-box",
+                border: "1px solid blue",
+                backgroundColor: "white",
                 color: "red",
                 top: 0,
                 transform: `translateY(${pokemon.offsetTop})px`,
@@ -185,10 +202,10 @@ console.log("scrollTop-->", scrollTop);
             >
               <div>{pokemon.name}</div>
               {/* <img src={pokemon.image} alt={`${pokemon.name}`} /> */}
-            </div> 
-          : <div key={pokemon.name}>Scrolling...</div>
+            </div>
+          );
         })}
-        </div> 
+        {/* </div> */}
         {isFetching && <div>Loading...</div>}
       </div>
     </>
