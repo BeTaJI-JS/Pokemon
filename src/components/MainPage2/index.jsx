a739b5343ffe7c157301ad3c3b114160ad612541
BeTaJI <lilysh994@mail.ru>
2024-03-21 15:56:52 +0300

    1) пофиксил окончательно дергание скролла и кучу запросов при скролле 2) добавил стили через styled components на компоненты

--- src/components/MainPage2/index.jsx
+++ src/components/MainPage2/index.jsx
@@ -1,16 +1,13 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
-// hook для навигации по роутам
+
 import { Link, useNavigate } from "react-router-dom";
 
-// лого для ссылки на главную страницу
 import logo from "../../assets/pokedex_logo.png";
-
-// хук для получения данных
 import { useGetItemsQuery } from "../../store/api";
 
-// стили для компонентов
-import { Wrapper, WrapperContent } from "./styles";
+
+import { PokemonCard, Wrapper } from "./styles";
 
 // высота одного элемента в пикселях
 const itemHeight = 40;
@@ -27,13 +24,8 @@ const threshold = 600;
 export const MainPage = () => {
   // массив с данными для отображения
   const [fullData, setFullData] = useState([]);
-
-  // индекс элемента, с которого начинать отображение. Изменяется при скроле
   const [startIndex, setStartIndex] = useState([]);
-
-  console.log("startIndex--->", startIndex);
-
-  console.log("fullData--->>>", fullData);
+  const [scrollProcess, setScrollProcess] = useState(false);
 
   // смещение для запроса новых данных
   const [offset, setOffset] = useState(0);
@@ -43,17 +35,14 @@ export const MainPage = () => {
 
   const navigate = useNavigate();
 
-  // данные для отображения
   const { data = null, isFetching } = useGetItemsQuery({ limit, offset }); // приходит только имя и ссылка
 
-  // добавляем данные в массив при получении новой порции
   useEffect(() => {
     if (data?.results?.length > 0) {
       setFullData((prev) => [...prev, ...data.results]);
     }
   }, [data?.results]);
 
-  // хук для запуска перехода по ссылке при клике на элемент списка
   const clickHandler = useCallback(
     (id) => {
       navigate(`/pokemon/${id}`);
@@ -64,13 +53,10 @@ export const MainPage = () => {
   // скролинг
   useEffect(() => {
     const scrollElement = scrollElementRef.current;
-
-    // если элемент не найден, то не выполняем код
     if (!scrollElement) {
       return;
     }
 
-    // обработчик скрола
     const onScroll = (e) => {
       // высота элемента
       // const rangeEnd = scrollElementRef?.current?.scrollTop + containerHeight;
@@ -89,98 +75,54 @@ export const MainPage = () => {
 
       // если достигли нижнего порога, загружаем еще порцию данных
       // console.log("e.target.scrollHeight ", e.target.scrollHeight);
+
       if (
         e.target.clientHeight + e.target.scrollTop >=
         e.target.scrollHeight - threshold
       ) {
-        setOffset((prevOffset) => prevOffset + limit);
+        setScrollProcess(true)
       }
-      setStartIndex(startIndex);
+       setStartIndex(startIndex);
     };
 
-    // подключаем обработчик скрола к элементу
     scrollElement.addEventListener("scroll", onScroll);
 
     // очищаем обра
     return () => {
       scrollElement.removeEventListener("scroll", onScroll);
     };
-  }, []);
-  // const onScroll = useCallback(
-  //   (e) => {
-  //     // const rangeEnd = scrollElementRef?.current?.scrollTop + containerHeight;
-  //     let startIndex = Math.ceil(
-  //       scrollElementRef?.current?.scrollTop / itemHeight
-  //     );
-  //     // let endIndex = Math.ceil(rangeEnd / itemHeight);
+  }, [scrollElementRef]);
 
-  //     startIndex = Math.max(0, startIndex);
-  //     // endIndex = Math.min(fullData.length - 1, endIndex);
+  // юз эфект для добалвения данных
+  useEffect(()=> {
+    if(scrollProcess){
+      setOffset((prevOffset) => prevOffset + limit);
+    }
+  },[scrollProcess])
 
-  //     console.log("e.target.scrollHeight ", e.target.scrollHeight);
-  //     if (
-  //       e.target.clientHeight + e.target.scrollTop >=
-  //       e.target.scrollHeight - threshold
-  //     ) {
-  //       setOffset((prevOffset) => prevOffset + limit);
-  //     }
-  //     setStartIndex(startIndex);
-
-  //   },
-  //   []
-  // );
-
-  console.log(
-    "scrollElementRef.current.scrollHeight=========>>>",
-    scrollElementRef?.current?.scrollHeight
-  );
+  useEffect(()=> {
+    setScrollProcess(false)
+  },[fullData])
 
   return (
     <>
       <div>
-        <Link to="/">
+        <Link to="/pokemon">
           <img alt="home" src={logo} title="home" />
         </Link>
       </div>
-      {/* <h1>ПОКЕМОНЫ ЕПТА</h1> */}
-      {/* <div
-        ref={scrollElementRef}
-        style={{
-          border: "3px solid red",
-          height: containerHeight,
-          overflowY: "scroll",
-          position: "relative",
-        }}
-        // onScroll={(e) => onScroll(e)}
-      > */}
-      <Wrapper ref={scrollElementRef}>
-        {/* <WrapperContent> */}
-        {fullData?.slice(startIndex, startIndex + limit)?.map((pokemon) => {
-          // console.log("el", el);
-          // const pokemon = fullData[el.index];
-          // console.log("pokemon--->>>", pokemon);
+      <Wrapper containerHeight={containerHeight} ref={scrollElementRef}>
+        <div style={{ height: itemHeight * startIndex }} />
+        {fullData
+        // ?.slice(startIndex, startIndex + limit) //пока без слайса - не рабоатет как надо сука
+        .map((pokemon) => {
           return (
-            <div
-              key={pokemon.name}
-              style={{
-                backgroundColor: "white",
-                border: "1px solid blue",
-                boxSizing: "border-box",
-                color: "red",
-                height: itemHeight,
-                top: 0,
-                // transform: `translateY(${el.offsetTop})px`,
-              }}
-              // value={el.offsetTop}
-              onClick={() => {
-                clickHandler(pokemon.name);
-              }}
-            >
+            <PokemonCard itemHeight={itemHeight} key={pokemon.name} onClick={() => clickHandler(pokemon.name)}>
               <div>{pokemon.name}</div>
-            </div>
+            </PokemonCard>
           );
         })}
-        {/* </WrapperContent> */}
+        <div style={{ height: itemHeight * (fullData.length - startIndex - limit) }} />
         {isFetching && <div>Loading...</div>}
       </Wrapper>
     </>
--- src/components/MainPage2/styles.js
+++ src/components/MainPage2/styles.js
@@ -2,15 +2,18 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   border: 3px solid red;
-  // height: containerHeight;
-  max-height:700px;
+  max-height: ${props => props.containerHeight}px;
   overflow-y: scroll;
   position: relative;
   display: flex;
   flex-direction: column;
   `;
 
-export const WrapperContent = styled.div`
-  display: flex;
-  flex-direction: column;
+export const PokemonCard = styled.div`
+  background-color: white;
+  border: 1px solid blue;
+  box-sizing: border-box;
+  color: red;
+  height: ${props => props.itemHeight};
+  top: 0;
 `;
